b545ef26490644f6da08f9b163e03ebdd5e62636
ausi1997.com <ausafen1018@gmail.com>
2021-05-27 10:17:25 +0530

    header

--- corona-dashboard/src/App.js
+++ corona-dashboard/src/App.js
@@ -1,10 +1,15 @@
-
+import React from 'react';
+import {Box} from '@material-ui/core';
+import logo from './assets/covid.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    Corona Virus Dashboard
+    <Box className='box'>
+    <Box className='box-inner'>COVID-19 PANADEMIC</Box>
+    <img style={{width:450}} src={logo} alt="covid"></img>
+    </Box>
     </div>
   );
 }
